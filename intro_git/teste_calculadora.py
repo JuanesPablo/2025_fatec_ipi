f1c8b3b43a11435702b2aeafa22069ddebcebb39
Juan Pablo <Jpmandarino@hotmail.com>
2025-08-15 20:48:34 -0300

    test(ap01-git): testa a subtracao

--- intro_git/teste_calculadora.py
+++ intro_git/teste_calculadora.py
@@ -5,3 +5,5 @@ soma = calculadora.somar(a, b)
 print(f'A soma de {a} + {b} é {soma}')
 subtração = calculadora.subtrair(a, b)
 print(f'A subtração de {a} - {b} é {subtração}')
+def s_multiplicar(a, b):
+    return calculadora.multiplicar(a, b)   
